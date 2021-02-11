56f3ad68da1d309934e8d516ebef0c306e3c96d9
Jaiyde <60797584+Jaiyde@users.noreply.github.com>
2021-02-11 09:25:30 -0800

    Update MAStandingStatics.m

--- Code/Matlab/Analysis/MAStandingStatics.m
+++ Code/Matlab/Analysis/MAStandingStatics.m
@@ -4,3 +4,19 @@ Static Analysis
 Standing Position
 %}
 
+%% Define Needed Dimensions (D) for Limbs, Joints, Actuators, and Attachment Points
+DFEM   = [0, 8.63]             %IN
+DTIB   = [6.1, 6.1]            %IN
+DFOOT  = [1.46, 6.19]          %IN
+DH_K   = [0, 8.63]             %IN
+DH_A   = [6.1 ,14.73]          %IN
+DAP_KE = [0.41, 0.41]          %IN
+DAP_KF = [0.43, 0.43]          %IN
+DAP_AE = [0.53, 0]             %IN
+DAP_AF = [0, 0.58]             %IN
+
+
+%% Sum of Forces
+
+
+%% Sum of Moments (Torques) Per Joint
