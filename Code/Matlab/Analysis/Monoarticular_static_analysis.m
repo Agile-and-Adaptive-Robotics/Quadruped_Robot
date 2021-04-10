f18245482af0d986dc60d16b644c3da6b4d055d3
krnacik <krnacik@pdx.edu>
2021-04-09 19:22:22 -0700

    updates

--- Code/Matlab/Analysis/Monoarticular_static_analysis.m
+++ Code/Matlab/Analysis/Monoarticular_static_analysis.m
@@ -14,10 +14,10 @@ l3 = 6.5 * (0.0254);                % [m] Limb length 3 (inches to meters conver
 g = 9.8;                            % [m/s^2]
 
 % Define length of actuators
-L_AF = (4:0.5:8) * (0.0254);        % [m] Ankle flexor length (inches to meters conversion)
-L_AE = (4:0.5:8) * (0.0254);        % [m] Ankle extensor length (inches to meters conversion)
-L_KF = (6:0.5:10) * (0.0254);       % [m] Knee flexor length (inches to meters conversion)
-L_KE = (6:0.5:10) * (0.0254);       % [m] Knee extensor length (inches to meters conversion)
+L_AF = (6:0.25:7.5) * (0.0254);        % [m] Ankle flexor length (inches to meters conversion)
+L_AE = (6:0.25:7.5) * (0.0254);        % [m] Ankle extensor length (inches to meters conversion)
+L_KF = (7:0.5:10) * (0.0254);       % [m] Knee flexor length (inches to meters conversion)
+L_KE = (7.5:0.5:10) * (0.0254);       % [m] Knee extensor length (inches to meters conversion)
 
 % Define actuator masses based on linear density estimate of 1.698 kg/m
 m_AF = 1.698 * L_AF;                % [kg] Ankle flexor mass
@@ -58,22 +58,22 @@ S = 1;                              % [0,1]
 %% Computing static force requirements
 
 % Knee flexor: Compute torque from gravity at a maximally flexed position
-T_KF_leg = (l2/2) * (m2 + m_AE + m_AF) * g;    % [Nm] Torque from tibia frame and lower limb actuators
-T_KF_ankle = (l2 + l3/2) * m3 * g;             % [Nm] Torque from ankle, at extended position
-T__KF_encoder = l2 * me * g;                   % [Nm] Torque from encoder at ankle
-Tg_KF = T_KF_leg + T_KF_ankle + T__KF_encoder; % [Nm] Total torque on knee from gravity
+T_KF_leg = (l2/2) * (m2 + m_AE + m_AF) * g;               % [Nm] Torque from tibia frame and lower limb actuators
+T_KF_ankle = (l2 + l3/2) * m3 * g;                        % [Nm] Torque from ankle, at extended position
+T__KF_encoder = l2 * me * g;                              % [Nm] Torque from encoder at ankle
+Tg_KF = T_KF_leg + T_KF_ankle + T__KF_encoder;            % [Nm] Total torque on knee from gravity
 
 % Knee extensor: Compute torque from gravity at a maximally extended position
 T_KE_leg = (l2/2) * (m2 + m_AE + m_AF) * g * cosd(60);    % [Nm] Torque from tibia frame and lower limb actuators
-T_KE_ankle = (l2 + l3 * cosd(60)/2) * m3 * g * cosd(60);  % [Nm] Torque from ankle, at extended position
-T__KE_encoder = l2 * me * g * cosd(60);                   % [Nm] Torque from encoder at ankle
-Tg_KE = T_KE_leg + T_KE_ankle + T__KE_encoder;            % [Nm] Total torque on knee from gravity
+T_KE_ankle = (l2 + l3 * cosd(60)/2) * m3 * g * cosd(60);  % [Nm] Torque from foot, at extended position
+T_KE_encoder = l2 * me * g * cosd(60);                    % [Nm] Torque from encoder at ankle
+Tg_KE = T_KE_leg + T_KE_ankle + T_KE_encoder;             % [Nm] Total torque on knee from gravity
 
 % Ankle flexor: Compute torque from gravity at a maximally flexed position
-Tg_AE = (l3 / 2) * (m3) * g;                    % [Nm] Torque from flexed ankle position
+Tg_AE = (l3 / 2) * (m3) * g;                              % [Nm] Torque from flexed ankle position
 
 % Ankle extensor: Compute torque from gravity at a maximally extended position
-Tg_AF = (l3 / 2) * (m3) * g;                    % [Nm] Torque from extended ankle position
+Tg_AF = (l3 / 2) * (m3) * g;                              % [Nm] Torque from extended ankle position
 
 
 
@@ -83,13 +83,13 @@ Tg_AF = (l3 / 2) * (m3) * g;                    % [Nm] Torque from extended ankl
 key_KF = strings(1,length(L_KF)); 
 
 % Setup the plot
-fig = figure('Color', 'w'); hold on, grid on, xlabel('Attachment Radius [mm]'), ylabel('Required Pressure [kPa]'), title('Monoarticular Knee Flexor: Required Pressure')
+fig = figure('Color', 'w'); hold on, grid on, xlabel('Attachment Radius [mm]'), ylabel('Required Pressure [kPa]'), title('Monoarticular Knee Flexor: Required Pressure'), ylim([350 800])
 
 % Plot the pressure required to resist gravity vs muscle attachment radii for each resting muscle length
 for n = 1:length(L_KF)                  % Iterate through each resting muscle length
    
     % Compute muscle strain at max flexion
-    k_KF = (r_KF * sqrt(2)) ./ L_KF(n);	% [-] Knee flexor strain
+    k_KF = (r_KF * pi() / 2) ./ L_KF(n);	% [-] Knee flexor strain
     
     % Remove strains that exceed max strain
     k_KF(k_KF > k_max) = [];            % [-] Knee flexor strain updated
@@ -106,15 +106,13 @@ for n = 1:length(L_KF)                  % Iterate through each resting muscle le
     figure(1)                           % Knee flexor plot
     plot((r_KF(1:length(k_KF))*1000), (P_KF/1000), '-', 'LineWidth', 3)
     key_KF(n) = sprintf('Actuator length: %.1f inch', (L_KF(n)/0.0254));
-    
+
 end
 
-% Add a line on the plot representing max pressure available
-figure(1)                              % Knee flexor plot
+% Add a line on the plot representing max pressure available 
 yline(620, '--r', 'LineWidth', 3);
 
-% Add the legend
-figure(1)                              % Knee flexor plot
+% Add the legend                             % Knee flexor plot
 legend(key_KF)
 
 %% Plot relationship between necessary knee extensor actuator pressure and attachment radii, varying actuator lengths
@@ -123,13 +121,13 @@ legend(key_KF)
 key_KE = strings(1,length(L_KE)); 
 
 % Setup the plot
-fig = figure('Color', 'w'); hold on, grid on, xlabel('Attachment Radius [mm]'), ylabel('Required Pressure [kPa]'), title('Monoarticular Knee Extensor: Required Pressure')
+fig = figure('Color', 'w'); hold on, grid on, xlabel('Attachment Radius [mm]'), ylabel('Required Pressure [kPa]'), title('Monoarticular Knee Extensor: Required Pressure'), ylim([350 800])
 
 % Plot the pressure required to resist gravity vs muscle attachment radii for each resting muscle length
 for n = 1:length(L_KE)                  % Iterate through each resting muscle length
    
     % Compute muscle strain at max flexion
-    k_KE = (r_KE * sqrt(2)) ./ L_KE(n);	% [-] Knee flexor strain
+    k_KE = (r_KE * pi()/2) ./ L_KE(n);	% [-] Knee flexor strain
     
     % Remove strains that exceed max strain
     k_KE(k_KE > k_max) = [];            % [-] Knee flexor strain updated
@@ -168,7 +166,7 @@ fig = figure('Color', 'w'); hold on, grid on, xlabel('Attachment Radius [mm]'), 
 for n = 1:length(L_AF)                  % Iterate through each resting muscle length
    
     % Compute muscle strain at max flexion
-    k_AF = (r_AF * sqrt(2)) ./ L_AF(n); % [-] Ankle extensotr strain
+    k_AF = (r_AF * pi()/2) ./ L_AF(n); % [-] Ankle extensotr strain
     
     % Remove strains that exceed max strain
     k_AF(k_AF > k_max) = [];            % [-] Ankle extensor strain updated
@@ -207,7 +205,7 @@ fig = figure('Color', 'w'); hold on, grid on, xlabel('Attachment Radius [mm]'), 
 for n = 1:length(L_AE)                  % Iterate through each resting muscle length
    
     % Compute muscle strain at max flexion
-    k_AE = (r_AE * sqrt(2)) ./ L_AE(n); % [-] Ankle extensotr strain
+    k_AE = (r_AE * pi()/2) ./ L_AE(n); % [-] Ankle extensotr strain
     
     % Remove strains that exceed max strain
     k_AE(k_AE > k_max) = [];            % [-] Ankle extensor strain updated
