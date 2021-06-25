1d27088958e4543749cfecf0a96044580c2e200e
jsamm2 <65787678+jsamm2@users.noreply.github.com>
2021-06-24 23:08:57 -0700

    data processing changes

--- Code/Matlab/Dynamic leg/Current Version/Data_Processing.m
+++ Code/Matlab/Dynamic leg/Current Version/Data_Processing.m
@@ -1,7 +1,26 @@
 clear;
 close all;
 
-prompt = 'Enter Joint Data:' ;
-fileName = input(prompt,'s');
-fileName1 = [fileName, '.mat'];
-load(fileName,'-mat') 
+%prompt = 'Enter Joint Data:' ;
+%fileName = input(prompt,'s');
+%fileName1 = [fileName, '.mat'];
+%load(fileName,'-mat') 
+load('golden4.mat');
+
+figure
+plot(Trial4);
+
+[DogLegDataKnee] = Trial1(:,1);
+[DogLegDataHip] = Trial4(:,2);
+
+plot(DogLegDataKnee);
+
+baudRate = 115200;
+RPM = 7500;
+
+
+
+
+
+
+
