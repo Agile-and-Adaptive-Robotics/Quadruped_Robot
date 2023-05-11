6e9ee1f47a4afdc5d319d0ef04b81993aacb1cf6
Cody Scharzenberger <Cody.Scharzenberger@gmail.com>
2023-05-11 13:17:46 -0700

    Removed unnecessary new folder.

--- Code/Matlab/Analysis/New folder/ForwardEulerStep.m
+++
@@ -1,9 +0,0 @@
-function U = ForwardEulerStep(U, dU, dt)
-
-% This function performs a single forward Euler step.
-
-% Estimate the simulation states at the next time step.
-U = U + dt*dU;
-
-end
-
--- Code/Matlab/Analysis/New folder/vecangle.m
+++
@@ -1,7 +0,0 @@
-function theta = vecangle(u, v)
-
-% Compute the angle between the given vectors.
-theta = atan2( norm(cross(u, v)), dot(u, v) );
-
-end
-
