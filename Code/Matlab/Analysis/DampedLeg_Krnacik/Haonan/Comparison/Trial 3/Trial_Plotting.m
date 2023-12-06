6f687ba62473b22123b17a321b2bea0c9faa6c1c
hnzheng96 <115669366+hnzheng96@users.noreply.github.com>
2023-12-05 23:49:04 -0800

    trial plotting
    I hate these encoder wires

---
+++ Code/Matlab/Analysis/DampedLeg_Krnacik/Haonan/Comparison/Trial 3/Trial_Plotting.m
@@ -0,0 +1,49 @@
+load 'GS_ankle_trial1.mat';
+figure
+hold on
+plot(data(:,4),data(:,3))
+plot(data(:,4),data(:,2))
+plot(data(:,4),data(:,1))
+hold off
+legend('ankle','knee','hip')
+
+load 'GS_trial2.mat'
+figure
+hold on
+plot(data(:,4),data(:,3))
+plot(data(:,4),data(:,2))
+plot(data(:,4),data(:,1))
+hold off
+
+load 'GS_trial3.mat'
+figure
+hold on
+plot(data(:,4),data(:,3))
+plot(data(:,4),data(:,2))
+plot(data(:,4),data(:,1))
+hold off
+
+load 'GS_trial4.mat'
+figure
+hold on
+plot(data(:,4),data(:,3))
+plot(data(:,4),data(:,2))
+plot(data(:,4),data(:,1))
+hold off
+
+load 'GS_trial5.mat'
+figure
+hold on
+plot(data(:,4),data(:,3))
+plot(data(:,4),data(:,2))
+plot(data(:,4),data(:,1))
+hold off
+
+load 'GS_trial6.mat'
+figure
+hold on
+plot(data(:,4),data(:,3))
+plot(data(:,4),data(:,2))
+plot(data(:,4),data(:,1))
+hold off
+
