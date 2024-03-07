4826cdbad182af2be44fae6bcd61e2ef3d0154ed
hnzheng96 <115669366+hnzheng96@users.noreply.github.com>
2024-03-07 15:50:53 -0800

    updated step response code
    updated step response code

--- Code/Matlab/Analysis/DampedLeg_Krnacik/Haonan/3DPrinted_Torsion_Spring/torqueTest/singleResponseTesting.m
+++ Code/Matlab/Analysis/DampedLeg_Krnacik/Haonan/3DPrinted_Torsion_Spring/torqueTest/singleResponseTesting.m
@@ -1,10 +1,10 @@
 data = springData.data.S2L4LT4ST37T.T200Nmm.CW.loading.average;
-mgr = springData.data.S2L4LT4ST37T.T200Nmm.torque;
+mgr = springData.data.S2L4LT4ST37T.T200Nmm.torque/1000;
 t = linspace(0,(length(data)-1)/100,length(data));
-I1 = 6.6066;        % [kg*mm^2]     inertia of the pulley from Solidworks
+I1 = 6.6066/1000^2; % [kg*m^2]      inertia of the pulley from Solidworks
 qq = 150;
 
-r = 20.4;           % [mm]          radius of the pulley
+r = 20.4/1000;      % [m]           radius of the pulley
 m = 1;              % [kg]          mass hung from pulley
 
 dataCropped = deg2rad(data(1:qq,1));
@@ -12,7 +12,8 @@ tCropped = t(1,1:qq);
 
 %%
 tr = risetime_ek(dataCropped,tCropped);
-ts = settime(dataCropped,tCropped); 
+ts = settime(dataCropped,tCropped);
+ts2 = settime(data,t);
 zeta = dampingRatio(tr,ts);
 omegan = (4.5*zeta)/ts;
 
@@ -22,16 +23,14 @@ k1 = (I1+(m*(r^2)))*omegan^2;         % [kg*m^2/s^2] spring constant of the spri
 s = tf('s');
 
 k2 = mgr/dataCropped(end);
-% k2 = mgr/0.1816;
-% omegan2 = sqrt(k2/(I1+(m*(r^2))));
-omegan2 = sqrt(k2/(I1));
+omegan2 = sqrt(k2/(I1+(m*(r^2))));
 zeta2 = ts*omegan2/4.5;
-% zeta2 = 3.2/(ts*omegan);
 b2 = 2*(I1+(m*(r^2)))*zeta2*omegan2;
 
-fun1 = mgr/((I1+(m*(r^2)))*s^2 + b1*s + k1);
-% fun2 = mgr/((I1+(m*(r^2)))*s^2 + b2*s + k2);
-fun2 = mgr/((I1)*s^2 + b2*s + k2);
+Tc = 20;
+
+fun1 = (mgr-0.01)/((I1+(m*(r^2)))*s^2 + b1*s + k1);
+fun2 = mgr/((I1+(m*(r^2)))*s^2 + b2*s + k2);
 
 figure
 plot(tCropped,dataCropped)
@@ -47,4 +46,5 @@ xlim([0 1.5])
 figure
 step(fun2)
 xlabel('Time (s)')
+xlim([0 1.5])
 
